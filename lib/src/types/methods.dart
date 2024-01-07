8fa434bfecb3e22bd52101bb29ccce874ae224d2
swedesjs <babaevvladislav828@gmail.com>
2024-01-07 12:18:38 +0300

    unused types removed

--- lib/src/types/methods.dart
+++ lib/src/types/methods.dart
@@ -1,8 +1,2 @@
-/// Тип результата вызова метода ВК
-typedef MethodTypeReturn = Future<Map<String, dynamic>>;
-
-/// Тип параметров метода ВК
-typedef MethodTypeParams = Map<String, Object>;
-
 /// Классическая карта значений, JSON
 typedef ClassicMap = Map<String, dynamic>;
