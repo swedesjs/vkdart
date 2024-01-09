6d7f6d398cc5ec439d5313e1e70a9229a153f91d
swedesjs <babaevvladislav828@gmail.com>
2024-01-10 00:56:18 +0300

    update: delete attachments structures

--- lib/vkdart.dart
+++ lib/vkdart.dart
@@ -1,10 +1,8 @@
-// ignore: lines_longer_than_80_chars
 /// This is a library that has many possibilities for interacting with the VK API
 /// and has a number of auxiliary functions
 library vkdart;
 
 export 'src/api.dart';
 export 'src/exceptions/api_exception.dart';
-export 'src/structures/attachements/attachments.dart';
 export 'src/types/methods.dart';
 export 'src/vkdart_base.dart';
