b7f78cb629aa8d08e4576e0a015483089765650c
swedesjs <babaevvladislav828@gmail.com>
2024-01-15 21:41:38 +0300

    Models for attachments have been created.

--- lib/model.dart
+++ lib/model.dart
@@ -1,4 +1,6 @@
 /// Various models used by VKDart
 library vkdart.model;
 
+export 'src/vkdart/model/attachments/attachment.dart';
+
 part 'src/vkdart/model/update.dart';
---
+++ lib/src/vkdart/model/attachments/attachment.dart
@@ -0,0 +1,71 @@
+import 'package:vkdart/vkdart.dart' show VkDartException;
+
+/// Base class Attachment
+///
+/// See https://vk.com/dev/objects/attachments
+abstract class AttachmentModel {
+  // ignore: public_member_api_docs
+  AttachmentModel(this.payload);
+
+  /// Payload.
+  final Map<String, dynamic> payload;
+
+  /// Attachment type.
+  String get type => payload['type'];
+
+  /// Attachment object.
+  Map<String, dynamic> get attachmentObject => payload[type];
+}
+
+/// Model Custom Attachment.
+class CustomAttachmentModel extends AttachmentModel {
+  // ignore: public_member_api_docs
+  CustomAttachmentModel(super.payload);
+
+  /// Parses an attachment from a string.
+  /// Example:
+  /// ```dart
+  /// CustomAttachmentModel.fromString('photo-1_2_ACCESS_KEY');
+  /// ```
+  factory CustomAttachmentModel.fromString(String attachment) {
+    final parseAttachmentRe =
+        RegExp(r'([a-z_]+)(-?\d+)_(\d+)_?(\w+)?', multiLine: true);
+
+    if (!parseAttachmentRe.hasMatch(attachment)) {
+      throw VkDartException('Incorrect attachment!');
+    }
+
+    final match = parseAttachmentRe.firstMatch(attachment)!;
+    final type = match[1]!;
+
+    final attachmentPayload = <String, dynamic>{
+      'type': type,
+      type: {
+        'owner_id': int.parse(match[2]!),
+        'id': int.parse(match[3]!),
+        if (match[4] != null) 'access_key': match[4],
+      }
+    };
+
+    return CustomAttachmentModel(attachmentPayload);
+  }
+
+  /// Attachment owner identifier.
+  int get ownerId => attachmentObject['owner_id'];
+
+  /// Attachment ID.
+  int get id => attachmentObject['id'];
+
+  /// Attachment access key.
+  String? get accessKey => attachmentObject['access_key'];
+
+  @override
+  String toString() =>
+      '$type${ownerId}_$id${accessKey != null ? '_$accessKey' : ''}';
+}
+
+/// Model External Attachment.
+class ExternalAttachmentModel extends AttachmentModel {
+  // ignore: public_member_api_docs
+  ExternalAttachmentModel(super.payload);
+}
