bae43828c64165df672fae516436700ae01e0730
swedesjs <babaevvladislav828@gmail.com>
2024-02-03 13:01:44 +0300

    refactoring code.

--- lib/vkontakte.dart
+++ lib/vkontakte.dart
@@ -3,3 +3,5 @@ library vkdart.vkontakte;
 
 export 'src/vkontakte/api.dart';
 export 'src/vkontakte/model/attachments/attachment.dart';
+export 'src/vkontakte/model/market_order.dart';
+export 'src/vkontakte/model/message.dart';
