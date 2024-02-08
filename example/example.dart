6f5a9c3a49300b9c3aba106d488c974c4fc36150
swedesjs <babaevvladislav828@gmail.com>
2024-02-08 21:19:30 +0300

    change: example.

--- example/example.dart
+++ example/example.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_declarations, avoid_print
-
 import 'package:vkdart/vkdart.dart';
 
 void main() async {
@@ -7,50 +5,24 @@ void main() async {
   // if the fetcher parameter is not specified, Longpoll will be used by default
   final vkdart = VkDart('accessToken', groupId: 123);
 
-  // Use callback
-  // final callback = Callback(
-  //  secretKey: 'mySecretKey', // optional
-  //  confirmationHandler: (_) async => 'confirimationCode',
-  //  );
+  // Use callback API
+  // final webhook = await Webhook.createHttpServer(
+  //  secretKey: 'mySecretKey',
+  //  confirmation: 'confirmationCode',
+  // );
   //
-  // final vkdart = VkDart('accessToken', event: Event(), fetcher: callback);
+  // final vkdart = VkDart('accessToken', event: Event(), fetcher: webhook);
 
   // message_new, message_edit, message_reply
-  vkdart.onMessage().where((event) => event.isNew).listen((event) {
-    print('A new message has arrived!\n\n'
-        'From ID: ${event.senderId}\n'
-        'From Type: ${event.senderType.name}\n'
-        'Dialog ID: ${event.peerId}\n'
-        'Dialog Type: ${event.peerType.name}\n'
-        'Outbox: ${event.isOutbox}');
+  vkdart.onMessage().where((event) => event.isUser).listen((event) {
+    vkdart.messages.send({
+      'peer_id': 1,
+      'message': 'Hello world!',
+      'random_id': 0,
+    });
   });
 
-  // message_event
-  vkdart.onMessageEvent().listen((event) {
-    print('The user clicked on the Callback button!\n'
-        'Dialog ID: ${event.peerId}\n'
-        'UserID: ${event.userId}'
-        '${event.eventPayload != null ? '\nPayload: ${event.eventPayload}' : ''}'
-        '\nEventID: ${event.eventId}');
-  });
-
-  vkdart.onUnsupportedEvent().listen((event) {
-    print('An unsupported event has arrived!\n'
-        'It is necessary to inform the chat https://t.me/vk_dart\n\n'
-        'Type of event ${event.eventType}\n'
-        'Event Object: ${event.object}');
-  });
-
-  vkdart
-      .onComment()
-      .where((event) => event.isPhotoComment && event.isNew)
-      .listen((event) {
-    print('Commented on the photo!\n\n'
-        'Owner ID: ${event.ownerId}\n'
-        'Photo ID: ${event.objectId}'
-        '${event.attachments.isNotEmpty ? '\nAttachments: ${event.attachments}' : ''}');
-  });
-
+  // ignore: avoid_print
   await vkdart.start().then((_) => print('Longpoll API run!'));
 
   // use API
