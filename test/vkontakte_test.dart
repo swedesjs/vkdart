087a5e3bf19b044009c279bfd31828e0b113b059
swedesjs <babaevvladislav828@gmail.com>
2024-02-18 00:27:43 +0300

    Change: tests.

--- test/vkontakte_test.dart
+++ test/vkontakte_test.dart
@@ -1,24 +1,28 @@
 import 'package:dotenv/dotenv.dart';
 import 'package:shouldly/shouldly.dart';
 import 'package:test/test.dart';
-import 'package:vkdart/vkdart.dart';
 import 'package:vkdart/vkontakte.dart';
 
 void main() {
   final env = DotEnv(includePlatformEnvironment: true)..load();
-  final vkdart = VkDart(env['TOKEN']!, groupId: int.parse(env['GROUP_ID']!));
+  final vkontakte = Vkontakte(
+    token: env['TOKEN']!,
+    language: LangApi.ru,
+    version: '5.131',
+  );
 
   group('Testing methods VK', () {
     group('check `request` function', () {
       test('should produce a positive result', () async {
-        final users = await vkdart.request('users.get', {'user_id': 'durov'});
+        final users =
+            await vkontakte.request('users.get', {'user_id': 'durov'});
 
         users.should.beOfType<List<dynamic>>();
       });
 
       test('should display an error', () async {
         await Should.throwAsync<ApiException>(
-            () => vkdart.request('users.report', {}));
+            () => vkontakte.request('users.report', {}));
       });
     });
   });
